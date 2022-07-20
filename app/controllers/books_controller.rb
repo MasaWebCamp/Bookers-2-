473273846f6180e8e425f5a621cfa79f6e26b0b0
MasaWebCamp <108528197+MasaWebCamp@users.noreply.github.com>
2022-07-20 10:24:42 +0000

    [Update]Books showアクションの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,19 @@ class BooksController < ApplicationController
   end
 
   def show
+    @new_book = Book.new
     @book = Book.find(params[:id])
+    @books = Book.all
+    @user = current_user
+  end
+  
+  def edit
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
   end
 
   private
