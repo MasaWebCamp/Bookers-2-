dbc402c6a164091cf670943159058de1a9eb10af
MasaWebCamp <108528197+MasaWebCamp@users.noreply.github.com>
2022-07-20 12:28:55 +0000

    [Add]プロフィール画像からUser Showページへの移行、titleからBook Showページへの移行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    @user.name = current_user.name
-    @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
   end
 
   def edit
